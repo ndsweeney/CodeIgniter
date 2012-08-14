dc8f247fb60d44d31b355542a97c3e62d0b8d816
Nathan Sweeney <ndsweeney@gmail.com>
2012-08-14 22:25:37 +0100

    test database

--- application/config/database.php
+++ application/config/database.php
@@ -48,12 +48,12 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['hostname'] = 'db427658237.db.1and1.com';
+$db['default']['username'] = 'dbo427658237';
+$db['default']['password'] = 'ho5pita1';
+$db['default']['database'] = 'test';
 $db['default']['dbdriver'] = 'mysql';
-$db['default']['dbprefix'] = '';
+$db['default']['dbprefix'] = '_codeigniter';
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
 $db['default']['cache_on'] = FALSE;
