b6de36a39bc33f776131331892e9fab863ec9d0e
Nathan Sweeney <ndsweeney@gmail.com>
2012-08-15 18:50:53 +0100

    test

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://ndsweeney/sandpit/CodeIgniter';
+$config['base_url']	= 'http://ndsweeney.co.uk/sandpit/CodeIgniter/';
 
 /*
 |--------------------------------------------------------------------------
